d4c6c37ea0a0a60c66927ad0adc7578e44d1dff9
Satyam <satya12100@hotmail.com>
2016-10-11 00:54:29 +0530

    admin new design

--- application/views/admin/category_manage.php
+++ application/views/admin/category_manage.php
@@ -1,123 +1,56 @@
-<?php
+<div id="content">
+	<section>
+		<div class="section-header">
+			<h3 class="text-standard"><i class="fa fa-fw fa-arrow-circle-right text-gray-light"></i> Category List</h3>
+		</div>
+	<div class="section-body">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="box">
+					<div class="box-body table-responsive">
+						<div id="datatable1_wrapper" class="dataTables_wrapper" role="grid">
+							<div id="datatable1_length" class="dataTables_length">
+								<table id="datatable1" class="table table-striped table-hover dataTable" aria-describedby="datatable1_info">
+									<thead>
+										<tr role="row">
+											<th class="sorting" role="columnheader" tabindex="0" aria-controls="datatable1" rowspan="1" colspan="1"  style="width: 10%;"></th>
+											<th class="sorting" role="columnheader" tabindex="0" aria-controls="datatable1" rowspan="1" colspan="1"  style="width: 30%;">Name</th>
+											<th class="sorting" role="columnheader" tabindex="0" aria-controls="datatable1" rowspan="1" colspan="1"  style="width: 30%;">File Name</th>
+											<th class="sort-numeric sorting" role="columnheader" tabindex="0" aria-controls="datatable1" rowspan="1" colspan="1"  style="width: 30%;">Actions</th>
+										</tr>
+									</thead>
+								
+									<tbody role="alert" aria-live="polite" aria-relevant="all">
+									<?php 
 
-$user=$this->session->userdata('user');
+										$i=1;
 
-?>
+										$statusClass=array("bg-red","bg-blue");
 
-      <div class="content-wrapper">      
-
-         <section class="">         
-
-            <div>
-
-              <div class="box">
-
-                <div class="box-header">                
-
-                  <h3 class="box-title">Category</h3>
-
-                </div><!-- /.box-header -->
-
-                <div class="box-body table-responsive no-padding">
-
-                  <table id="example1" class="table table-bordered table-hover">
-
-                    <thead>
-
-                    <tr>
-
-                      <th width="10%"></th>
-
-                      <th width="20%">Name</th>
-
-                      <th width="20%">File Name</th>
-
-                      <th width="20%">Action</th>
-
-                    </tr>
-
-                    </thead>
-
-                    <tbody>
-
-                    <?php 
-
-                    $i=1;
-
-                    $statusClass=array("bg-red","bg-blue");
-
-                    foreach ($list as $val) {?>
-                    <tr>
-                      <td><?php echo $i++;?></td>
-                      <td><?php echo $val['txt_name'];?></td>
-                      <td><img src="<?php echo base_url()."".$val['txt_filepath'];?>" alt="Smiley face" height="42" width="42"></td>                      
-                        <td>
-                          <a href="<?php echo site_url().'/admin/editcategory/'.$val['int_category_id']?>" class="btn btn-primary">Edit</a>
-                          <a href="<?php echo site_url().'/admin/deletedcategory/'.$val['int_category_id']?>" class="btn btn-danger">Delete</a>
-                      </td>                      
-                    </tr>
-                    <?php }?>
-
-                    </tbody>
-
-                  </table>
-
-                  
-
-                </div><!-- /.box-body -->
-
-              </div><!-- /.box -->
-
-            </div>
-
-
-
-<style>
-
-.datepicker{
-
-  z-index:9999 !important;
-
-}
-
-</style>
-
-          
-
-<script src="<?php echo base_url();?>plugins/datatables/jquery.dataTables.min.js"></script>
-
-<script src="<?php echo base_url();?>plugins/datatables/dataTables.bootstrap.min.js"></script>
-
-<script>
-
-  $(document).ready(function(){
-
-
-
-      $('#example1').DataTable({
-
-          // "paging": true,
-
-          // "lengthChange": false,
-
-          // "searching": false,
-
-          // "ordering": true,
-
-          // "info": true,
-
-          // "autoWidth": false
-
-        });
-
-  });
-
-  </script>
-
-
-
-
-
-        </section> 
-
-      </div>
+										foreach ($list as $val) {?>
+										<tr class="<?php echo ($i%2==0)?'gradeX odd':'radeC even'; ?>">
+										  <td><?php echo $i++;?></td>
+										  <td><?php echo $val['txt_name'];?></td>
+										  <td><img src="<?php echo base_url()."".$val['txt_filepath'];?>" alt="Smiley face" height="42" width="42"></td>                      
+											<td>
+											  <a href="<?php echo site_url().'/admin/editcategory/'.$val['int_category_id']?>" class="btn btn-primary">Edit</a>
+											  <a href="<?php echo site_url().'/admin/deletedcategory/'.$val['int_category_id']?>" class="btn btn-danger">Delete</a>
+										  </td>                      
+										</tr>
+									<?php }?>
+									</tbody>
+								</table>
+							</div>
+						</div>
+					</div><!--end .box-body -->
+				</div><!--end .box -->
+			</div><!--end .col-lg-12 -->
+		</div>
+		<!-- END DATATABLE 1 -->
+		
+		
+		
+	</div>
+</section>
+	</div><!--end #content-->		
+	<!-- END CONTENT -->
