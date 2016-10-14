4bd7a0361bf3b6fee39b966ad2c3a1e9eebb1fb5
Satyam <satya12100@hotmail.com>
2016-10-14 23:03:30 +0530

    changes

--- application/views/artist/header_home.php
+++ application/views/artist/header_home.php
@@ -1097,7 +1097,7 @@
         <link rel='stylesheet'  href='<?php echo base_url(); ?>assets/challwe_css/bootstrap.css' type='text/css' media='all' />
         <link rel='stylesheet' href='<?php echo base_url(); ?>assets/challwe_css/style.css' type='text/css' media='all' />
         <link rel='stylesheet'   href='fonts.googleapis.com/css?family=Open+Sans%3A300italic%2C400italic%2C600italic%2C300%2C400%2C600&amp;subset=latin%2Clatin-ext&amp;ver=4.5.4' type='text/css' media='all' />
-        <!--<script type='text/javascript' src='<?php echo base_url(); ?>js/jqueryb8ff.js?ver=1.12.4'></script>-->
+        <script type='text/javascript' src='<?php echo base_url(); ?>js/jqueryb8ff.js?ver=1.12.4'></script>
         <script type='text/javascript' src='<?php echo base_url(); ?>js/jquery-migrate.min330a.js?ver=1.4.1'></script>
         <script type='text/javascript'>
             /* <![CDATA[ */
