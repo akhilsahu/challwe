1e07730ca43d7c167e8a687a4aaac8d9aa2440c8
Satyam <satya12100@hotmail.com>
2016-11-13 13:49:39 +0530

    changes

--- application/views/artist/header_home.php
+++ application/views/artist/header_home.php
@@ -40,3 +40,76 @@
         <script type="text/javascript">var ajaxurl = 'wp-admin/admin-ajax.html';</script>
         <style type="text/css">.recentcomments a{display:inline !important;padding:0 !important;margin:0 !important;}</style>
     </head>
+	<body class="home-page home page page-id-33 page-template-default admin-bar no-customize-support  slimvideo no-js">
+        <div id="ts-loading-preload">
+            <div class="preloader-center"></div>
+        </div>
+        <div id="wrapper" class=" animsition " data-header-align="top">
+            <header id="header" class="row">
+                <div class="col-lg-12">
+                    <div data-parallax="no" data-scroll-btn="no" data-alignment="top"  class="site-section  has-row-mask "  style=" background-color: #2f2a3e;  color: #ffffff;  background-image:url('images/header_bg.jpg') ;  background-position: left center;  background-attachment: scroll;  background-repeat: no-repeat;  margin-top: 0px;  margin-bottom: 0px;  padding-top: 50px;  padding-bottom: 50px;  background-size: cover; " ><div class='row-mask' style='background-color:#41395d;opacity:0.7'></div><div class="container"><div class="row"><div style="background-color: transparent;color: inherit;background-position: left;background-attachment: scroll;background-repeat: repeat;background-size: auto;padding-top: 0px;padding-right: 20px;padding-left: 20px;padding-bottom: 0px;text-align: auto;" class="col-lg-3 col-md-3">
+                                    <div class="row content-block ">
+                                        <div class="col-lg-12 text-left" style="max-width: 120px;">
+                                            <a href="<?php echo site_url(); ?>" class="logo">
+                                                <img src="<?php echo base_url(); ?>assets/images/challwe-logo.png" alt="Slimvideo" style="width: 195px; height: auto;"/>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div style="background-color: transparent;color: inherit;background-position: left;background-attachment: scroll;background-repeat: repeat;background-size: auto;padding-top: 0px;padding-right: 20px;padding-left: 20px;padding-bottom: 0px;text-align: auto;" class="col-lg-6 col-md-6">
+                                    <div class="row content-block "><div class="col-lg-12 col-md-12 col-sm-12">
+                                            <div id="searchbox" class="text-left">
+                                                <a href="#" class="search-trigger">
+                                                    <i class="icon-search"></i>
+                                                    <span>click to search</span>
+                                                </a>
+                                                <div class="hidden-form-search">
+                                                    <form role="search" method="get" class="search-form" action="#">
+                                                        <fieldset>
+                                                            <input class="input" name="s" type="text" id="keywords" value="type anything and press enter" onfocus="if (this.value == 'type anything and press enter') {
+                                                                        this.value = '';
+                                                                    }" onblur="if (this.value == '') {
+                                                                                this.value = 'type anything and press enter';
+                                                                            }" />
+                                                            <input type="submit" class="searchbutton" name="search" value="Search" />
+                                                            <i class="icon-search"></i>
+                                                        </fieldset>
+                                                    </form>
+                                                    <a href="#" class="search-close"><i class="icon-cancel"></i></a>
+                                                </div>
+                                            </div>
+                                        </div></div>
+                                </div>
+                                <div style="background-color: transparent;color: inherit;background-position: left;background-attachment: scroll;background-repeat: repeat;background-size: auto;padding-top: 0px;padding-right: 20px;padding-left: 20px;padding-bottom: 0px;text-align: auto;" class="col-lg-3 col-md-3">
+                                    <div class="row content-block "><div class="col-lg-12">
+                                            <div class="ts-user-header-profile align-right">
+                                                <?php if ($user['logged_in']) { ?>
+                                                    <div class="user-mini-avatar">
+                                                        <a href="#"><img src="<?php echo ($user['txt_profile_image']) ? base_url() . $user['txt_profile_image'] : base_url() . 'assets/images/avatar-placeholder.png' ?>" class="icon_user"></a>
+                                                    </div>
+                                                    <div class="user-info">
+                                                        <div class="dropdown">
+                                                            <div class="ts-login-button" style="cursor: pointer;">
+                                                                <span data-toggle="dropdown" href="#"><?php echo $user['txt_fname'] . $user['txt_lname'] ?></span>
+                                                                <span class="caret"></span>
+                                                                <ul class="dropdown-menu">
+                                                                    <li><a href="<?php echo site_url(); ?>/wallet/mytransections"><i class=""></i>Wallet (<?php echo $user['int_challwe_coins']; ?>)</a></li>
+                                                                    <li><a href="<?php echo site_url() ?>/artist/dashboard"><i class=""></i>My Account</a></li>
+                                                                    <?php if ($user['login_type'] == 'web') { ?>
+                                                                        <li><a href="<?php echo site_url() ?>/user/signoutArt" style="font-weight: bold;"><i class="icon-right-arrow"></i>Logout</a></li>
+                                                                    <?php } else { ?>
+                                                                        <li><a href="<?php echo site_url() ?>/user/facebooklogout" style="font-weight: bold;"><i class="icon-right-arrow"></i>Logout</a></li>
+                                                                    <?php } ?>
+                                                                </ul>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                <?php } else { ?>
+                                                    <div class="user-mini-avatar">
+                                                        <a href="#"><i class="icon-user"></i></a>
+                                                    </div>
+                                                    <div class="user-info">
+                                                        <div class="ts-login-button 1"><a id="ts-show-login-modal" href="#">Login</a></div>
+                                                        <div class="ts-register-button"><a href="#">or <span>register</span></a></div>
+                                                    </div>
+                                                <?php } ?>
