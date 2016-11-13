36b99c4d3d5e1ba0047c02ea06cd9ab8ce691444
Satyam <satya12100@hotmail.com>
2016-11-13 13:41:00 +0530

    changes

--- application/views/artist/header_home.php
+++ application/views/artist/header_home.php
@@ -1,1 +1,43 @@
 <?php echo "sdsad";exit;?>
+<!DOCTYPE html>
+<html lang="en-US" prefix="og: http://ogp.me/ns#">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="profile" href="http://gmpg.org/xfn/11" />
+        <link rel="pingback" href="xmlrpc.php" />
+        <title>Welcome to official website Challwe</title>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Varela+Round:400,400italic,700&amp;subset=latin" type="text/css" media="all" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Varela+Round:400,400italic,700&amp;subset=latin" type="text/css" media="all" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Varela+Round:400,400italic,700&amp;subset=latin" type="text/css" media="all" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Varela+Round:400,400italic,700&amp;subset=latin" type="text/css" media="all" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Varela+Round:400,400italic,700&amp;subset=latin" type="text/css" media="all" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Varela+Round:400,400italic,700&amp;subset=latin" type="text/css" media="all" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,400italic,700&amp;subset=latin" type="text/css" media="all" />
+        <link rel='stylesheet' href='<?php echo base_url(); ?>assets/challwe_css/admin-bar.min.css' type='text/css' media='all' />
+        <link rel='stylesheet' href='<?php echo base_url(); ?>assets/challwe_css/dashicons.min.css' type='text/css' media='all' />
+        <link rel='stylesheet' href='<?php echo base_url(); ?>assets/challwe_css/buddypress.min.css' type='text/css' media='screen' />
+        <link rel='stylesheet' href='<?php echo base_url(); ?>assets/challwe_css/animsition.css' type='text/css' media='all' />
+        <link rel='stylesheet' href='<?php echo base_url(); ?>assets/challwe_css/redfont.css' type='text/css' media='all' />
+        <link rel='stylesheet' href='<?php echo base_url(); ?>assets/challwe_css/widgets.css' type='text/css' media='all' />
+        <link rel='stylesheet'  href='<?php echo base_url(); ?>assets/challwe_css/bootstrap.css' type='text/css' media='all' />
+        <link rel='stylesheet' href='<?php echo base_url(); ?>assets/buddypress/css/buddypress.min.css' type='text/css' media='all' />
+        <link rel='stylesheet' href='<?php echo base_url(); ?>assets/challwe_css/style.css' type='text/css' media='all' />
+        <link rel='stylesheet'   href='fonts.googleapis.com/css?family=Open+Sans%3A300italic%2C400italic%2C600italic%2C300%2C400%2C600&amp;subset=latin%2Clatin-ext&amp;ver=4.5.4' type='text/css' media='all' />
+        <script type='text/javascript' src='<?php echo base_url(); ?>js/jqueryb8ff.js?ver=1.12.4'></script>
+        <script type='text/javascript' src='<?php echo base_url(); ?>js/jquery-migrate.min330a.js?ver=1.4.1'></script>
+        <script type='text/javascript'>
+            var BP_Confirm = {"are_you_sure": "Are you sure?"};
+        </script>
+        <script type='text/javascript'>
+            var BP_DTheme = {"accepted": "Accepted", "close": "Close", "comments": "comments", "leave_group_confirm": "Are you sure you want to leave this group?", "mark_as_fav": "Favorite", "my_favs": "My Favorites", "rejected": "Rejected", "remove_fav": "Remove Favorite", "show_all": "Show all", "show_all_comments": "Show all comments for this thread", "show_x_comments": "Show all %d comments", "unsaved_changes": "Your profile has unsaved changes. If you leave the page, the changes will be lost.", "view": "View"};
+            /* ]]> */
+        </script>
+        <link rel='api.w.org/' href='wp-json/index.html' />
+        <link rel="EditURI" type="application/rsd+xml" title="RSD" href="xmlrpc0db0.php?rsd" />
+        <link rel="wlwmanifest" type="application/wlwmanifest+xml" href="wp-includes/wlwmanifest.xml" />
+        <meta name="generator" content="WordPress 4.5.4" />
+        <link rel='shortlink' href='index.html' />
+        <script type="text/javascript">var ajaxurl = 'wp-admin/admin-ajax.html';</script>
+        <style type="text/css">.recentcomments a{display:inline !important;padding:0 !important;margin:0 !important;}</style>
+    </head>
